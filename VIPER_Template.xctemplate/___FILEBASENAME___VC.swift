ebab030523ab156bda5f1d92906177d890d5e57c
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2020-10-29 12:03:45 -0600

    Se agregan archivos

---
+++ VIPER_Template.xctemplate/___FILEBASENAME___VC.swift
@@ -0,0 +1,21 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import Foundation
+import UIKit
+
+class ___VARIABLE_ModuleName___VC: UIViewController {
+    var presenter: ___VARIABLE_ModuleName___PresenterProtocol?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+///Protocolo para recibir datos del presenter.
+extension ___VARIABLE_ModuleName___VC: ___VARIABLE_ModuleName___ViewProtocol {
+}
